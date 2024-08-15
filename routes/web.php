3efdaa37800c48dd94f9ef5e561492925e0a9bf1
KenCode03 <suzuken03011996@gmail.com>
2024-08-16 05:11:15 +0900

    顧客関連

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -7,6 +8,8 @@ use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
 
+Route::resource('customers',CustomerController::class)
+->middleware('auth','verified');
 
 Route::resource('items',ItemController::class)
 ->middleware('auth','verified');
